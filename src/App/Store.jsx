f7609e9089dba858b5aca138ae5e651735870aad
Al Hassan <mateenmahihot@gmail.com>
2023-09-18 18:31:52 +0500

    Updated store

--- src/App/Store.jsx
+++ src/App/Store.jsx
@@ -1,10 +1,10 @@
+//Store For using Redux Toolkit 
+
 import { configureStore } from "@reduxjs/toolkit";
-import CounterReducer from "../Features/Counter/CounterSlice";
 import productReducer from "../Features/Counter/ProductSlice"
 
 export const store = configureStore({
     reducer:{
-        show:CounterReducer,
-        products:productReducer
+        products:productReducer,
     }
 })
