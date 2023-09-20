5e58d262c7051915f54ed19a3a5fe08c344c7b99
Al Hassan <mateenmahihot@gmail.com>
2023-09-20 19:51:15 +0500

    Miner Change

--- src/Components/Model.jsx
+++ src/Components/Model.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect} from "react";
 import Select from "react-select";
 import axios from "../Features/axios";
 import makeAnimated from "react-select/animated";
